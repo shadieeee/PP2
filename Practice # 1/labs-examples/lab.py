9825a30382a2e0e49575284bef44d28ff311bb63
shadieeee <s_ilyasov@kbtu.kz>
2026-01-27 18:35:25 +0500

    third 5 examples of 1 labs

--- Practice # 1/labs-examples/lab.py
+++ Practice # 1/labs-examples/lab.py
@@ -69,15 +69,53 @@ print(a.lower())
 
 # 11 Example
 
+a = input()
+if len(a) == 1:
+    print(f"{a} {a}")
+else:
+    print(f"{a[0]} {a[-1]}")
 
 # 12 Example
 
+a = input()
+
+print(f"{a[2:5]}")
 
 # 13 Example
+
+a = input()
+b = reversed(a)
+
+print("".join(b))
+
 # 14 Example
+
+a = input()
+b = input()
+
+print(f"Hello, {a}. You are {b} years old.Hello, Alice. You are 30 years old.")
+
 # 15 Example
+
+a = input()
+b = input()
+
+if b in a:
+    print("True")
+else:
+    print("False")
+
 # 16 Example
+
+a = input()
+b = input()
+
+print(f"{a}{b}")
+
 # 17 Example
+
 # 18 Example
+
 # 19 Example
+
 # 20 Example
