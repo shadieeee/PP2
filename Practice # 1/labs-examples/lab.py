cd307fe16313976b861217503f34dd83f82c0cf6
shadieeee <s_ilyasov@kbtu.kz>
2026-01-27 18:26:50 +0500

    first 5 examples of 1 Lab

---
+++ Practice # 1/labs-examples/lab.py
@@ -0,0 +1,56 @@
+# 1 Example
+
+a = input()
+print(f"Hello, {a}")
+
+# 2 Example
+
+a = input()
+b = input()
+
+print(f"{a}***{b}")
+
+# 3 Example
+
+a = str(input())
+
+if a.isdigit():
+    print("int")
+else:
+    print("str")
+
+# 4 Example
+
+a = input()
+b = input()
+
+print(a + b)
+
+# 5 Example
+
+a = input()
+b = input()
+
+print(a // b)
+print(a / b)
+
+# 6 Example
+# 7 Example
+# 8 Example
+# 9 Example
+
+
+# 10 Example
+
+# 11 Example
+
+# 12 Example
+# 13 Example
+# 14 Example
+# 15 Example
+# 16 Example
+# 17 Example
+# 18 Example
+# 19 Example
+
+# 20 Example
