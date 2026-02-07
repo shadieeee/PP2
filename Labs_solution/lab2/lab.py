2bbd698433abaf7bd6ddf81216f240f35bb8e997
shadieeee <s_ilyasov@kbtu.kz>
2026-02-07 14:09:56 +0500

    Update lab.py

--- Labs_solution/lab2/lab.py
+++ Labs_solution/lab2/lab.py
@@ -1,14 +1,9 @@
 # 1 example
-
-import sys
-from collections import Counter
-year = int(input())
-
-if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
+y = int(input())
+if ((y % 4 == 0 and y % 100 != 0) or y % 400 == 0):
     print("YES")
 else:
     print("NO")
-
 # 2 example
 
 n = int(input())
