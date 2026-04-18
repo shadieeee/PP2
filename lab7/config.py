0d45fe4ede167e6ac951515958b4fa730884e1eb
shadieeee <s_ilyasov@kbtu.kz>
2026-04-18 14:36:10 +0500

    wtvr

---
+++ lab7/config.py
@@ -0,0 +1,8 @@
+def get_db_config():
+    return {
+        "host": "localhost",
+        "database": "phonebook",
+        "user": "postgres",
+        "password": "homachka564",
+        "port": "5432"
+    }
